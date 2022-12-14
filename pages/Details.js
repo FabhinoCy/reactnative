c9f0d854aa5b99597ab52e2dbb45592dc0b2a861
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2022-12-14 11:14:51 +0100

    AsyncStorage

--- pages/Details.js
+++ pages/Details.js
@@ -1,16 +1,37 @@
-import React from "react"
+import React from "react";
 import {Text, View, Image, Button, StyleSheet, AsyncStorage} from "react-native";
 
 function Details({route}) {
 
-    const item= route.params.item
+    const item = route.params.item
+    const [favoris, setFavoris] = React.useState(false);
+
+    React.useEffect(() => {
+        AsyncStorage.getItem('favoris')
+            .then((value) => {
+                const favorisParsed = JSON.parse(value);
+                const itemAlreadyInFavoris = favorisParsed.find((itemInFavoris) => itemInFavoris.id === item.id);
+                if (itemAlreadyInFavoris) {
+                    setFavoris(true);
+                }
+            });
+    });
 
     const ajouterFavoris = async () => {
-        let students = [];
-        students.push({name: item.name,gender:item.gender});
-        console.log(students);
-        await AsyncStorage.setItem('favoris', JSON.stringify(students));
-        console.log('token', await AsyncStorage.getItem('favoris'));
+        const favoris = await AsyncStorage.getItem('favoris');
+        if (favoris !== null) {
+            const favorisParsed = JSON.parse(favoris);
+            const itemAlreadyInFavoris = favorisParsed.find((itemInFavoris) => itemInFavoris.id === item.id);
+            if (itemAlreadyInFavoris) {
+                console.log('Déjà dans les favoris');
+            } else {
+                favorisParsed.push(item);
+                await AsyncStorage.setItem('favoris', JSON.stringify(favorisParsed));
+                setFavoris(true);
+            }
+        } else {
+            await AsyncStorage.setItem('favoris', JSON.stringify([item]));
+        }
     }
 
     return (
@@ -19,7 +40,9 @@ function Details({route}) {
             <Text style={styles.nom}>{item.name}</Text>
             <Text>{item.status === 'Alive' ? 'Vivant' : 'Mort'} - {item.species === 'Human' ? 'Humain' : 'Alien'}</Text>
             <Text>{item.gender === 'Male' ? 'Masculin' : 'Féminin'}</Text>
-            <Button title="Ajouter aux favoris" onPress={ajouterFavoris} />
+            {
+                favoris === true ? '' : <Button title="Ajouter aux favoris" onPress={ajouterFavoris} />
+            }
         </View>
     );
 }
--- pages/Favoris.js
+++ pages/Favoris.js
@@ -1,23 +1,59 @@
 import React from "react"
-import {AsyncStorage, Text, View} from "react-native";
+import {AsyncStorage, Button, Image, StyleSheet, Text, View, ScrollView} from "react-native";
 
 function Favoris() {
 
     const [favoris, setFavoris] = React.useState([]);
+    const [favorisLength, setFavorisLength] = React.useState(0);
 
     React.useEffect(() => {
         AsyncStorage.getItem('favoris').then((value) => {
             setFavoris(JSON.parse(value));
         });
-        console.log(favoris);
-    }, []);
+        setFavorisLength(favoris.length);
+    });
+
+    const retirerItem = async (item) => {
+        // delete item from favoris
+        const favoris = await AsyncStorage.getItem('favoris');
+        // suppression de l'item
+        const favorisParsed = JSON.parse(favoris);
+        const favorisFiltered = await favorisParsed.filter((itemInFavoris) => itemInFavoris.id !== item.id);
+        AsyncStorage.setItem('favoris', JSON.stringify(favorisFiltered));
+    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Favoris</Text>
-            <Text>{favoris[0].name}</Text>
+            <ScrollView>
+                {
+                    favorisLength !== 0 ? favoris.map((item) => {
+                            return (
+                                <>
+                                    <Text key={item.id} style={styles.nom}>{item.name}</Text>
+                                    <Image source={{uri: item.image}} style={styles.image}/>
+                                    <Button title="Supprimer des favoris" onPress={() => {retirerItem(item)}} />
+                                </>
+                            )
+                        }
+                    ) : <Text>Vous n'avez enregistré aucun favoris</Text>
+                }
+            </ScrollView>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        minWidth: '70%',
+        height: 100
+    },
+    nom: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'green',
+        textAlign: 'center',
+        marginTop: 10
+    }
+});
 
 export default Favoris;
